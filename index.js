a5c8875accf57f6f1c6dab4572e8a908b778a88e
aregchatalyan <53829696+aregchatalyan@users.noreply.github.com>
2021-06-10 15:51:30 +0400

    v0.2

--- index.js
+++ index.js
@@ -26,15 +26,17 @@ async function* getFiles(dir) {
         const fileDir = path.parse(filePath).dir;
         const fileName = path.parse(filePath).name;
 
-        fs.mkdirSync(`${fileDir.replace('input', 'output')}`, {recursive: true});
-        fs.stat(filePath, (e, stat) => metadataSize = stat.size);
+        await fs.mkdir(`${fileDir.replace('input', 'output')}`, {recursive: true}, (err) => {if (err) throw err});
+        await fs.stat(filePath, (e, stat) => metadataSize = stat.size);
 
-        sharp(filePath)
-            .metadata((e, meta) => console.info({format: meta.format}, {width: meta.width}, {height: meta.height}, {size: byte(metadataSize)}, 'Before', fileName))
+        await sharp(filePath)
+            .metadata((e, meta) => meta ?
+                console.info({format: meta.format}, {width: meta.width}, {height: meta.height}, {size: byte(metadataSize)}, 'Before', fileName) :
+                console.log('Meta empty'))
             .withMetadata()
             .resize({width, height})[fileFormat]({quality})
             .toFile(`${fileDir.replace('input', 'output')}/${fileName}.${fileFormat}`)
             .then((info) => console.info({format: info.format}, {width: info.width}, {height: info.height}, {size: byte(info.size)}, 'After', fileName))
             .catch((err) => console.error(err));
     }
-})(2, 100, 3000, null); // on first launch, create input* and output* directories
+})(2, 100, 256, null); // on first launch, create input* and output* directories
