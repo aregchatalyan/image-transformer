35421a9c51fca7117da2b394951980931658c546
aregchatalyan <aregchatalian@gmail.com>
2024-07-14 02:52:51 +0400

    update

--- index.js
+++ index.js
@@ -1,48 +1,65 @@
-const path = require('path');
-const sharp = require('sharp');
-const {readdir, mkdir, stat} = require('fs').promises;
-const {error, info, log} = console;
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
 
-let metadataSize;
-const format = ['jpeg', 'png', 'webp', 'tiff', 'avif', 'heif'];
-
-const byte = (bytes) => {
-  if (bytes === 0) return '0';
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  return parseFloat((bytes / Math.pow(1024, i)).toFixed(1)) + ' ' + sizes[i];
+const logger = (meta, size, fileName) => {
+  console.log({
+    format: meta.format,
+    width: meta.width,
+    height: meta.height,
+    size: formatBytes(meta.size || size),
+    fileName
+  });
 }
 
-async function* getFiles(dir) {
-  const dirs = await readdir(dir, {withFileTypes: true});
-  for (const d of dirs) {
-    const res = path.resolve(dir, d.name);
-    d.isDirectory() ? yield* getFiles(res) : yield res;
+const getFilesList = async (dir, fileList = []) => {
+  const files = await fs.promises.readdir(dir);
+
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const stat = await fs.promises.stat(filePath);
+
+    if (stat.isFile()) {
+      fileList.push(filePath);
+    } else {
+      await getFilesList(filePath, fileList);
+    }
   }
+
+  return fileList;
 }
 
-const logs = (e, meta, fileName) => {
-  meta
-    ? info({format: meta.format}, {width: meta.width}, {height: meta.height}, {size: byte(metadataSize)}, 'Before', fileName)
-    : log('Meta empty');
+const formatBytes = (sizeInBytes) => {
+  if (sizeInBytes <= 0) return '0B';
+
+  const units = [ 'B', 'KB', 'MB', 'GB', 'TB' ];
+
+  const unitIndex = Math.floor(Math.log(sizeInBytes) / Math.log(1024));
+
+  return (sizeInBytes / Math.pow(1024, unitIndex)).toFixed(2) + units[unitIndex];
 }
 
-(async (formatId, quality, width, height) => {
-  for await (const filePath of getFiles('input')) {
-    const fileFormat = format[formatId];
-    const fileDir = path.parse(filePath).dir;
-    const fileName = path.parse(filePath).name;
+(async (input, format, quality, width, height) => {
+  if (!input) return;
 
-    await mkdir(`${fileDir.replace('input', 'output')}`, {recursive: true});
-    const {size} = await stat(filePath);
-    if (size) metadataSize = size;
+  const filesList = await getFilesList(input);
 
-    await sharp(filePath)
-      .withMetadata()
-      .metadata((e, meta) => logs(e, meta, fileName))
-      .resize({width, height})[fileFormat]({quality})
-      .toFile(`${fileDir.replace('input', 'output')}/${fileName}.${fileFormat}`)
-      .then((inf) => info({format: inf.format}, {width: inf.width}, {height: inf.height}, {size: byte(inf.size)}, 'After', fileName))
-      .catch((err) => error(err));
+  for (const filePath of filesList) {
+    const { name, dir } = path.parse(filePath);
+
+    await fs.promises.mkdir(dir.replace('input', 'output'), { recursive: true });
+    const { size } = await fs.promises.stat(filePath);
+
+    try {
+      const meta = await sharp(filePath)
+        .withMetadata()
+        .metadata((_, meta) => logger(meta, size, name))
+        .resize({ width, height })[format]({ quality })
+        .toFile(`${ dir.replace('input', 'output') }/${ name }.${ format }`);
+
+      logger(meta, meta.size, name);
+    } catch (e) {
+      console.error(e);
+    }
   }
-})(2, 100, 3840, null); // on first launch, create input* and output* directories
+})('input', 'jpeg', 100, 320, undefined);
